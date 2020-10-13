9fb99068b75a1618767ee1341dd5efb50c092f5b
iamarbaazkhan <arbaazansarkhan25@gmail.com>
2020-10-13 23:45:42 +0530

    updated the counter app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  //used hook
   const [count, setCount] = useState(0);
   return (
     <div className="App">
@@ -10,8 +11,14 @@ function App() {
       </header>
       <h2>Counter App starting from {count}</h2>
       <button onClick={() => setCount(0)}>Reset counter</button>
-      <button onClick={() => setCount(count + 1)}>Increase Counter</button>
-      <button onClick={() => setCount(count - 1)}> Decrease Counter</button>
+      {/* used ternary operator */}
+      <button onClick={() => (count > 10 ? "" : setCount(count + 1))}>
+        Increase Counter
+      </button>
+      <button onClick={() => (count < 1 ? "" : setCount(count - 1))}>
+        {" "}
+        Decrease Counter
+      </button>
     </div>
   );
 }
